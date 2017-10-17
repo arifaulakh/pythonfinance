d6a75752a44fa4a86d3efb1c03c781c9afde1d3a
arifaulakh <aular@utschools.ca>
2017-10-16 23:01:17 -0400

    Update cbaddress.py

--- coinbase/cbaddress.py
+++ coinbase/cbaddress.py
@@ -1,6 +1,6 @@
 from coinbase.wallet.client import Client
 
-client = Client(api_key = '5cP3Cov8sOJROEjt', api_secret = 'PUyjLtVwB7JGjSFu2tz4M9XQxzoP4Ryt', api_version='2017-09-16')
+client = Client(api_key = '#', api_secret = '#', api_version='2017-09-16')
 
 # Get your primary coinbase account
 primary_account = client.get_primary_account()
